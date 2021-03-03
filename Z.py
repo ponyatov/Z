4dfdd59ae8730d89a418c9a610cd205e9f4da373
Dmitry Ponyatov <dponyatov@gmail.com>
2021-03-03 23:12:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Z.py
+++ Z.py
@@ -4,13 +4,24 @@ import os,sys,re
 class Object:
     def __init__(self,V):
         self.value = V
+        self.slot = {}
         self.nest = []
     
     def __repr__(self): return self.dump()
     def test(self): return self.dump(test=True)
     def dump(self,cycle=[],depth=0,prefix='',test=False):
         def pad(depth): return '\n'+'\t'*depth
+        # head
         ret = pad(depth) + self.head(prefix,test)
+        # cycle
+        if not depth: cycle=[]
+        if self in cycle: return ret + ' _/'
+        else: cycle.append(self)
+        # slot{}s
+        for i in self.keys():
+            ret += self[i].dump(cycle,depth+1,f'{i} = ',test)
+        # nest[]ed
+        # sutree
         return ret
     def head(self,prefix='',test=False):
         hash = f' @{id(self):x}' if not test else ''
@@ -18,25 +29,61 @@ class Object:
     def tag(self): return f'{self.__class__.__name__.lower()}'
     def val(self): return f'{self.value}'
 
-files = '''
-apt.txt
-Makefile
-requirements.txt
-Z.py
-.vscode/settings.json
-tmp/.gitignore
-'''
+    def keys(self):
+        return sorted(self.slot.keys())
+    def __getitem__(self,key):
+        assert isinstance(key,str)
+        return self.slot[key]
+    def __setitem__(self,key,that):
+        assert isinstance(key,str)
+        assert isinstance(that,Object)
+        self.slot[key] = that ; return self
+    def __lshift__(self,that):
+        return self.__setitem__(that.tag(),that)
+    def __rshift__(self,that):
+        return self.__setitem__(that.val(),that)
 
-giti = '''
-*
-!.gitignore'
-'''
+    def html(self): return self.head()
 
 class Env(Object): pass
 
 glob = Env('global')
 
+class Meta(Object): pass
+
+meta = Meta('info') ; glob << meta
+
+class Module(Meta): pass
+
+meta['MODULE'] = MODULE = Module('metaL')
+
+class Title(Meta):
+    def html(self):
+        return f'<i>{self.value}</i>'
+
+meta['TITLE'] = TITLE = Title('object/graph database + web platform')
+
+class Author(Meta):
+    def html(self):
+        email = f' {self["email"].html()}' if 'email' in self.keys() else ''
+        return f'{self.val()}{email}'
+
+meta['AUTHOR'] = AUTHOR = Author('Dmitry Ponyatov')
+
 class Web(Object): pass
+
+class EMail(Web):
+    def html(self):
+        return f'&lt;<a href="mailto:{self.value}">{self.value}</a>&gt;'
+
+meta['EMAIL'] = EMAIL = EMail('dponyatov@gmail.com')
+AUTHOR << EMAIL
+
+class Url(Web):
+    def html(self):
+        return f'<a href="{self.value}">{self.value}</a>'
+
+meta['GITHUB'] = Url('https://github.com/ponyatov/Z')
 
 import flask
 
